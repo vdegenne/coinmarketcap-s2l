8b00fe7f949487a91b06fec1b8908d3d240e3bee
Valentin Degenne <vdegenne@gmail.com>
2024-11-20 14:50:04 +0100

    types

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,7 @@ import terser from '@rollup/plugin-terser';
 import ts from '@rollup/plugin-typescript';
 
 export default {
-	input: 'src/main.ts',
+	input: './lib/index.js',
 	output: {file: 'index.js', format: 'esm'},
-	plugins: [ts(), cjs(), json(), terser()],
+	plugins: [cjs(), json(), terser()],
 };
-
